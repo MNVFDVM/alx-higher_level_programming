92011e38cf1b46001afc40f94d49d1a5c78b87a1
medo <mohammedabdelhamed725@gmail.com>
2024-03-18 05:18:10 -0700

    er

---
+++ 0x00-python-hello_world/6-concat.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+s1 = "holberton"
+s2 = "School"
+s1 = s1 + " " + s2
+print(f"welcome to {s1}!")
---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+w = "Holberton"
+first_3 = w[:3]
+last_2 = w[-2:]
+middle = w[1:-1]
+print(f"First 3 letters: {first_3}")
+print(f"Last 2 letters: {last_2}")
+print(f"Middle word: {middle}")
