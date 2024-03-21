5bfa194637407a4e7cb8d8b4269ecbf2d8a9e5a8
medo <mohammedabdelhamed725@gmail.com>
2024-03-21 10:44:20 -0700

    gf

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,7 @@ def roman_to_int(roman_string):
         return 0
     t = 0
     n = 0
-    d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'c': 100, 'D': 500, 'M': 1000}
+    d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     for f in reversed(roman_string):
         n = d[f]
         t += n if t < n * 5 else -n
