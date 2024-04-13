93860a7a392c6b2ad2c522022cd57e6b091cb648
medo <mohammedabdelhamed725@gmail.com>
2024-04-12 19:44:38 -0700

    gf

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -16,6 +16,6 @@ def print_square(size):
     if size < 0:
         raise ValueError("size must be >= 0")
 
-    for i in range(size):
-        [print("#", end="") for j in range(size)]
+    for k in range(size):
+        [print("#", end="") for h in range(size)]
         print("")
