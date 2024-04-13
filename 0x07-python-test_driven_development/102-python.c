e11124755b3cd1fd10f61c996a0c0267f0960299
medo <mohammedabdelhamed725@gmail.com>
2024-04-12 19:37:46 -0700

    gtf

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -6,33 +6,23 @@
  */
 void print_python_string(PyObject *p)
 {
-	long int l;
-	wchar_t *wide_str;
+	long int length;
 
 	fflush(stdout);
 
 	printf("[.] string object info\n");
 	if (strcmp(p->ob_type->tp_name, "str") != 0)
 	{
-		printf("	[ERROR] Invalid String Object\n");
+		printf("  [ERROR] Invalid String Object\n");
 		return;
 	}
 
-	l = ((PyASCIIObject *)(p))->length;
+	length = ((PyASCIIObject *)(p))->length;
 
 	if (PyUnicode_IS_COMPACT_ASCII(p))
-		printf("	type: compact ascii\n");
+		printf("  type: compact ascii\n");
 	else
-		printf("	type: compact unicode object\n");
-
-	wide_str = PyUnicode_AsWideCharString(p, &l);
-	if (wide_str == NULL) {
-		printf("Failed to convert to wide character string\n");
-		return;
-	}
-
-	printf("	length: %ld\n", l);
-	wprintf(L"	value: %ls\n", wide_str);
-
-	PyMem_Free(wide_str);
+		printf("  type: compact unicode object\n");
+	printf("  length: %ld\n", length);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
 }
