4affa762d6268f7cc62ced29f7757acc17c1bb5e
medo <mohammedabdelhamed725@gmail.com>
2024-04-18 10:33:33 -0700

    gdf

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python3
 
 """Deffre frefref refre frefre frefer  referf ometry."""
+
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Rrergrefre ffrfre refer eometry."""
+	"""Rectangle class."""
 
-    def __init__(self, width, height):
-        """Intisdfef dfre fregle.
-        Args:
-            width (int): The wierfref erfre fref gle.
-            height (int): The herretr gfergfre gregrangle.
-        """
-        super().integer_validator("width", width)
-        self.__width = width
-        super().integer_validator("height", height)
-        self.__height = height
+	def __init__(self, width, height):
+		"""Initialize Rectangle.
+		Args:
+			width (int): The width of the rectangle.
+			height (int): The height of the rectangle.
+		"""
+		super(Rectangle, self).__init__()
+		super(Rectangle, self).integer_validator("width", width)
+		self.__width = width
+		super(Rectangle, self).integer_validator("height", height)
+		self.__height = height
 
-    def area(self):
-        """Retuergtr rgtr gftrg trgtrgtr gtgtrge."""
-        return self.__width * self.__height
+	def area(self):
+		"""Return the area of the rectangle."""
+		return self.__width * self.__height
 
-    def __str__(self):
-        """Returfre erfer ferf erferfre referfre ferfref er erferngle."""
-        string = "[" + str(self.__class__.__name__) + "] "
-        string += str(self.__width) + "/" + str(self.__height)
-        return string 
+	def __str__(self):
+		"""Return string representation of the rectangle."""
+		return "[{}] {}/{}".format(self.__class__.__name__, self.__width, self.__height)
