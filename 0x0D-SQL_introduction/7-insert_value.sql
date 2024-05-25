7e174cf8e0cd80556d73ef25be54c2a68e054760
medo <mohammedabdelhamed725@gmail.com>
2024-05-25 06:15:39 -0700

    er

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,4 @@
--- Insert values
+-- Insert values into the first_table
 -- Execute: cat 7-insert_value.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
+
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, 'Holberton School');
