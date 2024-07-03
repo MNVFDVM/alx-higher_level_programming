897841d3b13e88eb23f2f164a929a15429bff9c7
MNVFDVM <mohammedabdelhamed725@gmail.com>
2024-07-03 16:05:21 -0700

    fds

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -4,11 +4,11 @@ const { dict } = require('./101-data.js');
 const newDict = {};
 
 for (const userId in dict) {
-	const occurrences = dict[userId];
-	if (!newDict[occurrences]) {
-		newDict[occurrences] = [];
-	}
-	newDict[occurrences].push(userId);
+  const occurrences = dict[userId];
+  if (!newDict[occurrences]) {
+    newDict[occurrences] = [];
+  }
+  newDict[occurrences].push(userId);
 }
 
 console.log(newDict);
