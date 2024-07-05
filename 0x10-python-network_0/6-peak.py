71ab24880099b33f8ee6d27e266900a961e123db
MNVFDVM <mohammedabdelhamed725@gmail.com>
2024-07-04 21:05:30 -0700

    df

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,29 +1,18 @@
-#!/usr/bin/python3
-
-
 def find_peak(list_of_integers):
-
-    if list_of_integers is None or len(list_of_integers) == 0:
+    """Finds a peak element in a list of unsorted integers using a binary search approach."""
+    if not list_of_integers:
         return None
 
-    if len(list_of_integers) == 1:
-        return list_of_integers[0]
-
-    mid_idx = int(len(list_of_integers) / 2)
-
-    if mid_idx != len(list_of_integers) - 1:
-        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx] and\
-           list_of_integers[mid_idx + 1] < list_of_integers[mid_idx]:
-            return list_of_integers[mid_idx]
-    else:
-        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx]:
-            return list_of_integers[mid_idx]
+    def find_peak_util(nums, low, high):
+        mid = (low + high) // 2
+        # Check if mid element is a peak
+        if (mid == 0 or nums[mid - 1] <= nums[mid]) and (mid == len(nums) - 1 or nums[mid + 1] <= nums[mid]):
+            return nums[mid]
+        # If the left neighbor is greater, then there is a peak in the left half
+        elif mid > 0 and nums[mid - 1] > nums[mid]:
+            return find_peak_util(nums, low, mid - 1)
+        # If the right neighbor is greater, then there is a peak in the right half
         else:
-            return list_of_integers[mid_idx - 1]
+            return find_peak_util(nums, mid + 1, high)
 
-    if list_of_integers[mid_idx - 1] > list_of_integers[mid_idx]:
-        a_list = list_of_integers[0:mid_idx]
-    else:
-        a_list = list_of_integers[mid_idx + 1:]
-
-    return find_peak(a_list)
+    return find_peak_util(list_of_integers, 0, len(list_of_integers) - 1)
