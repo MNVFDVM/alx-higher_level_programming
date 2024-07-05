7a3ba42fe8c4667fb14205b5c920384e6297bc23
MNVFDVM <mohammedabdelhamed725@gmail.com>
2024-07-04 21:02:59 -0700

    fd

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -sX POST $1 -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -L
+# This script sends a POST request to the URL passed as an argument with specific parameters and displays the response body
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
