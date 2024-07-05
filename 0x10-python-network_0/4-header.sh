fa289b8ccfb3c31ca4f5d2f5fa7fa5d3d825384b
MNVFDVM <mohammedabdelhamed725@gmail.com>
2024-07-04 21:01:05 -0700

    df

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -sX GET $1 -H "X-HolbertonSchool-User-Id: 98" -L
+# This script sends a GET request to the URL passed as an argument with a custom header and displays the response body
+curl -s -H "X-School-User-Id: 98" "$1"
